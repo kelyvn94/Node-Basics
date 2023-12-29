21017c025f572768d4fe0204e2cb9aa8bf795b05
kelvyn94 <kmacharia584@gmail.com>
2023-12-29 17:55:21 +0300

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+//create a view engine
+app.set('view engine', 'ejs')
+
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get('/about', (req, res) => {
+    res.render('about');
+})
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+})
+
+app.use((req, res) => {
+    res.status(404).render('404');
+})

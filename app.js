2caadfa2a44f27239c7f8136bd89a8c18d4e36f0
kelvyn94 <kmacharia584@gmail.com>
2024-01-18 11:58:05 +0300

    added blogs file

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const blog = require('./views/models/blogs');
 
 const app = express();
+
+//connect to database
+const mongoURL = 'mongodb+srv://programmer:pro123@node-basics.nt7atf3.mongodb.net/Node-Basics?retryWrites=true&w=majority';
+mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => console.log('Database connected'))
+.catch((err) => console.log(err));
 
 //create a view engine
 app.set('view engine', 'ejs')
 
 app.listen(3001);
+
+app.get('/blogs', (req, res) => {
+    const blogs = new Blog({
+        title: 'My first blog',
+        author: 'Me',
+        body: 'This is my first blog'
+    });
+    blogs.save()
+    .then((result) => {
+        res.send(result);
+     })
+     .catch((err) => {
+        res.send(err);
+     });
+    })
+})
 
 app.get('/', (req, res) => {
     res.render('home');

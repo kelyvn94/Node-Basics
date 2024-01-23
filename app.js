d3a60e25997db1a3f37710a13dc80e63bf02d003
kelvyn94 <kmacharia584@gmail.com>
2024-01-23 09:10:54 +0300

    Creating models

--- app.js
+++ app.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const blog = require('./views/models/blogs');
+const prisma = require('./db');
 
 const app = express();
 
+app.listen(5000,()=>{
+console.log('listening')
+})
+
 //connect to database
-const mongoURL = 'mongodb+srv://programmer:pro123@node-basics.nt7atf3.mongodb.net/Node-Basics?retryWrites=true&w=majority';
-mongoose.connect(mongoURL,)
-.then((result) => console.log('Database connected'))
-.catch((err) => console.log(err));
+// const mongoURL = 'mongodb+srv://programmer:pro123@node-basics.nt7atf3.mongodb.net/Node-Basics?retryWrites=true&w=majority';
+// mongoose.connect(mongoURL,)
+// .then((result) => console.log('Database connected'))
+// .catch((err) => console.log(err));
 
 //create a view engine
 app.set('view engine', 'ejs');
 
-app.get('/blogs', (req, res) => {
-    const blogs = new Blog({
-        title: 'My first blog',
-        author: 'Me',
-        body: 'This is my first blog'
-    });
-    blogs.save()
-    .then((result) => {
-        res.send(result);
-     })
-     .catch((err) => {
-        res.send(err);
-     });
-    });
+app.get('/', async(req, res) => {
+    //res.render('home');
+   
 
-app.get('/', (req, res) => {
-    res.render('home');
+    const user=await prisma.user.findMany()
+  
+    res.send(user)
 })
 app.use(express.static ("Public"));
 app.get('/about', (req, res) => {
@@ -44,7 +39,7 @@ app.get('/contact', (req, res) => {
 app.get('/contant-us', (req, res) => {
     res.redirect('/about');
 })
-app.get('/Portforlio', (req, res) => {
+app.get('/Portfolio', (req, res) => {
     res.redirect('/about');
 })
 
